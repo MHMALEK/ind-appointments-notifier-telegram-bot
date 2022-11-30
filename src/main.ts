48aeeecf975dbf1100f03c8079266472af29cf9b
APLKLAX0932578 <x0932578@Mohammad-SPLXA-Y0G67RRY0P.local>
2022-11-30 18:55:51 +0100

    init project with appointment system

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,163 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { Bot, Context, session, SessionFlavor } from 'grammy';
+import { Menu, MenuRange } from '@grammyjs/menu';
+import fetch from 'node-fetch';
+import dotEnv from 'dotenv';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+dotEnv.config();
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+console.log('22222');
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
+const getIndServicesContent = async () => {
+  try {
+    const responseObj = await fetch(
+      `${process.env.IND_CONTENT_API_BASE_URL}/service-desk`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.IND_CONTENT_API_TOKEN}`,
+        },
+      },
+    );
+
+    const res: any = await responseObj.json();
+
+    const {
+      data: { attributes },
+    } = res;
+
+    return {
+      ...attributes,
+    };
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+// const startBot = async () => {};
+
+const { service_types, desks, service_list } = await getIndServicesContent();
+
+/** This is how the dishes look that this bot is managing */
+interface Dish {
+  id: string;
+  name: string;
 }
+
+interface SessionData {
+  selectedService: string;
+  selectedDesk: string;
+}
+type MyContext = Context & SessionFlavor<SessionData>;
+
+/**
+ * All known dishes. Users can rate them to store which ones are their favorite
+ * dishes.
+ *
+ * They can also decide to delete them. If a user decides to delete a dish, it
+ * will be gone for everyone.
+ */
+const dishDatabase: Dish[] = [
+  { id: 'pasta', name: 'Pasta' },
+  { id: 'pizza', name: 'Pizza' },
+  { id: 'sushi', name: 'Sushi' },
+  { id: 'entrct', name: 'Entrecôte' },
+];
+
+const bot = new Bot<MyContext>(
+  '5745105271:AAFDscbS35w00wT3SXk0c_u180YHSMts1U4',
+);
+
+bot.use(
+  session({
+    initial(): SessionData {
+      return { selectedService: null, selectedDesk: null };
+    },
+  }),
+);
+
+const mainText = 'Please select a service';
+const serviceMenu = new Menu<MyContext>('service');
+
+for (const serviceType of service_types) {
+  // console.log('serviceType', serviceType);
+  serviceMenu
+    .submenu(
+      { text: serviceType.label, payload: serviceType.service_code }, // label and payload,
+      'credits-menu',
+      (ctx) => {
+        ctx.editMessageText(selectService(serviceType.label), {
+          parse_mode: 'HTML',
+        }); // handler
+        ctx.session.selectedService = serviceType.service_code;
+      },
+    )
+    .row();
+}
+
+const selectService = (service: string) =>
+  `You have selected this service<b>${service}</b>`;
+
+const deskMenu = new Menu<MyContext>('desk');
+deskMenu.dynamic((ctx, range) => {
+  const service = ctx.match;
+  if (typeof service !== 'string') throw new Error('No service chosen!');
+  range // no need for `new MenuRange()` or a `return`
+    .text('a', (ctx) => ctx.reply('text'))
+    .row()
+    .back('Go Back');
+});
+
+const settings = new Menu('credits-menu');
+settings.dynamic((ctx, range) => {
+  const service = ctx.session.selectedService;
+  const desks = getIndDesksByService(service);
+
+  if (typeof service !== 'string') throw new Error('No service chosen!');
+  for (const desk of desks) {
+    // console.log('serviceType', serviceType);
+    range
+      .text(
+        { text: desk.label, payload: desk.code }, // label and payload
+        (ctx) => {
+          ctx.session.selectedDesk = ctx.match;
+          ctx.menu.close();
+          ctx.editMessageText(
+            selectDeskForService(
+              ctx.session.selectedService,
+              ctx.session.selectedDesk,
+            ),
+          ); // handler
+        },
+      )
+      .row();
+  }
+});
+
+const getIndDesksByService = (selectedService: any) => {
+  const indServiceData: any = service_types.filter(
+    (service: any) => service.service_code === selectedService,
+  );
+
+  return indServiceData[0].desks;
+};
+
+const selectDeskForService = (service: string, desk: string) =>
+  `You have selected this service<b>${service}</b> for desk <b>${desk}<b>`;
+
+serviceMenu.register(settings);
+
+bot.use(serviceMenu);
+
+bot.command('start', (ctx) =>
+  ctx.reply(mainText, { reply_markup: serviceMenu }),
+);
+bot.command('help', async (ctx) => {
+  const text =
+    'Send /start to see and rate dishes. Send /fav to list your favorites!';
+  await ctx.reply(text);
+});
+
+bot.catch(console.error.bind(console));
+bot.start();
+
+// startBot();
